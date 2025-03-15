67faffdbae331803eba0a0f5b080ba01b280c9e8
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2025-03-15 00:13:53 -0400

    Add function

--- supabase/migrations/20250314030801_target.sql
+++ supabase/migrations/20250314030801_target.sql
@@ -13,5 +13,16 @@ CREATE TABLE
 CREATE VIEW private.test_view AS
 SELECT id, username, age FROM private.test;
 
+CREATE OR REPLACE FUNCTION private.get_age_category(user_age INTEGER)
+RETURNS TEXT AS $$
+BEGIN
+  RETURN CASE
+    WHEN user_age < 18 THEN 'Minor'
+    WHEN user_age BETWEEN 18 AND 65 THEN 'Adult'
+    ELSE 'Senior'
+  END;
+END;
+$$ LANGUAGE plpgsql;
+
 -- Create index "profiles_search_idx" to table: profiles
 CREATE INDEX "test_search_idx" ON private.test ("username");

c960339ec17247f0ccca2cba596dd05fa3679256
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2025-03-15 00:15:34 -0400

    Add function

--- results/migra-diff.sql
+++ results/migra-diff.sql
@@ -2,6 +2,22 @@ alter table "private"."test" add column "age" integer;
 
 CREATE INDEX test_search_idx ON private.test USING btree (username);
 
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION private.get_age_category(user_age integer)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  RETURN CASE
+    WHEN user_age < 18 THEN 'Minor'
+    WHEN user_age BETWEEN 18 AND 65 THEN 'Adult'
+    ELSE 'Senior'
+  END;
+END;
+$function$
+;
+
 create or replace view "private"."test_view" as  SELECT test.id,
     test.username,
     test.age
--- results/pg-schema-diff.sql
+++ results/pg-schema-diff.sql
@@ -1,3 +1,18 @@
+-- HAS_UNTRACKABLE_DEPENDENCIES: Dependencies, i.e. other functions used in the function body, of non-sql functions cannot be tracked. As a result, we cannot guarantee that function dependencies are ordered properly relative to this statement. For adds, this means you need to ensure that all functions this function depends on are created/altered before this statement.
+CREATE OR REPLACE FUNCTION private.get_age_category(user_age integer)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  RETURN CASE
+    WHEN user_age < 18 THEN 'Minor'
+    WHEN user_age BETWEEN 18 AND 65 THEN 'Adult'
+    ELSE 'Senior'
+  END;
+END;
+$function$
+;
+
 ALTER TABLE "private"."test" ADD COLUMN "age" integer;
 
 -- INDEX_BUILD: This might affect database performance. Concurrent index builds require a non-trivial amount of CPU, potentially affecting database performance. They also can take a while but do not lock out writes.
